e98231b2719c92fc4ca72f7de81d237ff7b29b58
rakhaibnu <mrakha.gunawan@gmail.com>
2024-12-05 16:10:00 +0700

    add page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,28 @@
 <x-app-layout>
-    {{-- About Page --}}
-
+    <article class="prose">
+        <h1 class="text-center">About</h1>
+        <p> DevNotes is a knowledge-sharing platform designed for 
+            developers, technical writers, and tech enthusiasts. 
+            DevNotes provides a space for the tech community to 
+            share articles, tutorials, tips, and experiences 
+            around the world of software development, design, and 
+            digital innovation. We believe that sharing knowledge 
+            is the first step to creating better solutions and 
+            building a strong technology community. Find inspiration, 
+            learn something new, or share your insights in DevNotes.</p>
+        <p> The website was developed by a creative team consisting of
+            Ausath, Rakha, Ghita, Dira, and Callysta. With diverse 
+            backgrounds and expertise, we worked together to create 
+            DevNotes as a collaborative space that can inspire and 
+            empower the tech community. Our commitment is to deliver 
+            high-quality content and an intuitive and enjoyable 
+            user experience.</p>
+        <ul>
+            <li>2310512045 - Muhammad Rakha Ibnu Gunawan</li>
+            <li>2310512062 - Indira Dwi Febrian</li>
+            <li>2310512075 - Ausath Abdi Dzil Ikram</li>
+            <li>2310512078 - Ghita Cahya Ramadhanti</li>
+            <li>2310512079 - Callysta Cendikia Diba Putri</li>
+        </ul>
+    </article>     
 </x-app-layout>
